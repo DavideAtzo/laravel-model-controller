0e4e136dac72ba7dfad283940945859572998012
DavideAtzo <atzo9477@gmail.com>
2023-05-04 15:43:47 +0200

    giusto un po di stile con bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,19 +7,26 @@
          @vite('resources/js/app.js')
     </head>
     <body>
-        <h1>Movies</h1>
-        <ul>
-        @foreach ($movies as $movie)
-            <li>
-                <div>
-                    <p>Title: {{ $movie->title }}</a></p>
-                    <p>id: {{ $movie->id }}</p>
-                    <p>oiginal_title: {{ $movie->original_title }}</p>
-                    <p>date: {{ $movie->date }}</p>
-                    <p>vote: {{ $movie->vote }}</p>
+        
+        <div class="container">
+            <div class="row">
+                <div class="my-2">
+                    <h1>Movies</h1>
                 </div>
-            </li>
-        @endforeach
-        </ul>
+                <ul class="list-unstyled d-flex justify-content-between flex-wrap gap-2">
+                    @foreach ($movies as $movie)
+                        <li class="card col-3">
+                            <div class="card-body">
+                                <h5>Title: {{ $movie->title }}</h5>
+                                <p>id: {{ $movie->id }}</p>
+                                <p>oiginal_title: {{ $movie->original_title }}</p>
+                                <p>date: {{ $movie->date }}</p>
+                                <p>vote: {{ $movie->vote }}</p>
+                            </div>
+                        </li>
+                    @endforeach
+                    </ul>
+            </div>
+        </div>
     </body>
 </html>
